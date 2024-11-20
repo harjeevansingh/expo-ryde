107e519e859dd51bf0e1a213bba3d6ffde6bb29e
harjeevansingh <harjeevansingh01@gmail.com>
2024-11-20 09:55:25 +0530

    feat: intial flow setup for onboarding

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -33,6 +33,10 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
+      {/* <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        NOT hiding the header for now, as it's not working as expected. */}
+      <Stack.Screen name="(root)/(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="(root)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
   );
